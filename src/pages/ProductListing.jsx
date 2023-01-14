dfa0eef1968c340bb64ded694bf660413e676ff9
Md Shahazad Uddin <shanudjn@gmail.com>
2023-01-14 13:33:30 +0530

    chore: eslint formatting

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -3,22 +3,24 @@ import { getFilteredProducts } from "../utils/filtering";
 import { ProductCard } from "../components/ProductCard";
 import { Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 
-
 export const ProductsListing = ({ products }) => {
   const { filter } = useFilter();
   const filteredProducts = getFilteredProducts(products, filter);
   return (
     <>
-    {
-      (filteredProducts.length > 0) ? <SimpleGrid columns={4} spacing={5} width="75%" paddingX={"2rem"}>
-      {filteredProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </SimpleGrid> : <Flex width={"75%"} alignItems="center" justifyContent={"center"}><Heading as="h5" size="md">There are no items</Heading></Flex>
-    }
-    
-   
+      {filteredProducts.length > 0 ? (
+        <SimpleGrid columns={4} spacing={5} width="75%" paddingX={"2rem"}>
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Flex width={"75%"} alignItems="center" justifyContent={"center"}>
+          <Heading as="h5" size="md">
+            There are no items
+          </Heading>
+        </Flex>
+      )}
     </>
   );
 };
-
