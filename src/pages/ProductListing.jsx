f7f732dce827a34af9db8e77647b25a3adda3010
Md Shahazad Uddin <shanudjn@gmail.com>
2023-01-14 13:32:20 +0530

    minor : no item screen

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,18 +1,24 @@
 import { useFilter } from "../context/filterProvider";
 import { getFilteredProducts } from "../utils/filtering";
 import { ProductCard } from "../components/ProductCard";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 
 
 export const ProductsListing = ({ products }) => {
   const { filter } = useFilter();
   const filteredProducts = getFilteredProducts(products, filter);
   return (
-    <SimpleGrid  columns={4} spacing={5} width="75%" paddingX={"2rem"}>
+    <>
+    {
+      (filteredProducts.length > 0) ? <SimpleGrid columns={4} spacing={5} width="75%" paddingX={"2rem"}>
       {filteredProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
-    </SimpleGrid>
+    </SimpleGrid> : <Flex width={"75%"} alignItems="center" justifyContent={"center"}><Heading as="h5" size="md">There are no items</Heading></Flex>
+    }
+    
+   
+    </>
   );
 };
 
