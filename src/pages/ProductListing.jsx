bf6e14276fafd54b954e2853d91cadc6615af220
Md Shahazad Uddin <shanudjn@gmail.com>
2023-01-14 09:35:23 +0530

    change : use chakra css components

--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,17 +1,18 @@
 import { useFilter } from "../context/filterProvider";
 import { getFilteredProducts } from "../utils/filtering";
 import { ProductCard } from "../components/ProductCard";
+import { SimpleGrid } from "@chakra-ui/react";
 
 
 export const ProductsListing = ({ products }) => {
   const { filter } = useFilter();
   const filteredProducts = getFilteredProducts(products, filter);
   return (
-    <div className="grid-container" style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+    <SimpleGrid  columns={4} spacing={5} width="75%" paddingX={"2rem"}>
       {filteredProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
-    </div>
+    </SimpleGrid>
   );
 };
 
